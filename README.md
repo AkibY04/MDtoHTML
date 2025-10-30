cd86d4fe757aa7376bfd3745840d5749d773ac01
Akib Yeahia <akib.yeahia@stonybrook.edu>
2025-10-29 23:06:59 -0400

    Base set up

---
+++ README.md
@@ -0,0 +1,1 @@
+conda activate md2html-env
---
+++ md2html.py
@@ -0,0 +1,24 @@
+def convert_emphasis(text: str) -> str:
+    return
+
+def convert_paragraph(text: str) -> str:
+    return
+
+def convert_headings(text: str) -> str:
+    return
+
+def convert_ordered_list(text: str) -> str:
+    return
+
+def convert_unordered_list(text: str) -> str:
+    return
+
+def convert_code(text: str) -> str:
+    return
+
+def convert_link(text: str) -> str:
+    return
+
+def convert(text: str) -> str:
+    return
+
